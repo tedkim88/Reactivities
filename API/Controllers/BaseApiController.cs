3efe458809f5ed5e43e63a33d331b154be6debea
daejinKim <kdjalberta@gmail.com>
2025-04-25 23:11:49 -0600

    End of Section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+namespace API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+   
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
+
+
+
